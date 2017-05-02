a8583077905ca9c76055e51ac89a6c8d5aac5aae
denysnovoascmspain <denys.novoa@scmspain.com>
2017-05-02 15:37:15 +0200

    first commit

---
+++ src/main/java/core/HelloWorld.java
@@ -0,0 +1,17 @@
+package core;
+
+public class HelloWorld {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void printName() {
+    System.out.println("Spring 4 :Hello !" + name);
+  }
+}
